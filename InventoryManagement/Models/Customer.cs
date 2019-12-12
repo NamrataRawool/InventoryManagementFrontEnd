f78f9163da7355420f270c11271d08bcb22fcc46
Aditya Bhende <bhendeaditya@gmail.com>
2019-12-12 23:45:41 +0530

    - Added Category and Customer Models, Updated Product Model. - Added HTTPService[GET, POST] support. - Added Temporary Tests for HTTPService.

---
+++ InventoryManagement/Models/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Models
+{
+    public class CustomerBase
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string MobileNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public int TotalAmount { get; set; }
+
+        public int PendingAmount { get; set; }
+
+    }
+
+    public class CustomerPost : CustomerBase { }
+
+    public class CustomerGet : CustomerBase { }
+
+}
--- InventoryManagement/Program.cs
+++ InventoryManagement/Program.cs
@@ -1,4 +1,6 @@
-﻿using InventoryManagement.UI;
+﻿using InventoryManagement.Models;
+using InventoryManagement.Services.HTTP;
+using InventoryManagement.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,66 @@ namespace InventoryManagement
 {
     static class Program
     {
+
+        private static void TestGET()
+        {
+            // GET
+            var Categories = HTTPService.GET<List<CategoryGet>>("categories");
+            Console.WriteLine("Number of Products in DB: " + Categories.Count);
+
+
+            var Product = HTTPService.GET<ProductGet>("product/32");
+            Console.WriteLine(Product.Name);
+
+            var Customers = HTTPService.GET<List<CustomerGet>>("customers");
+            Console.WriteLine(Customers.Count);
+
+        }
+
+        private static void TestPOST()
+        {
+            bool postProduct = false;
+            if (postProduct)
+            {
+                List<string> files = new List<string>();
+                files.Add("C:/Users/ASUS ROG/Desktop/passport photo.jpg");
+
+                ProductPost pPost = new ProductPost();
+                pPost.Name = "test name";
+                pPost.Description = "test description";
+                pPost.RetailPrice = 100;
+                pPost.WholeSalePrice = 98;
+                pPost.CategoryID = 1;
+
+                var PostResponse = HTTPService.POST("product", pPost, files);
+                Console.WriteLine(PostResponse.Name);
+            }
+
+            bool postCategory = false;
+            if (postCategory)
+            {
+                CategoryPost cPost = new CategoryPost();
+                cPost.Name = "test name";
+                cPost.Description = "test Description";
+                cPost.CGST = 12.6;
+                cPost.SGST = 12.6;
+                cPost.Discount = 10;
+
+                var PostResponse = HTTPService.POST("category", cPost);
+                Console.WriteLine(PostResponse.Name);
+            }
+
+            bool postCustomers = true;
+            if (postCustomers)
+            {
+                CustomerPost cPost = new CustomerPost();
+                cPost.Name = "Aditya Bhende";
+
+                var PostResponse = HTTPService.POST("customer", cPost);
+                Console.WriteLine(PostResponse.Name);
+            }
+        }
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
